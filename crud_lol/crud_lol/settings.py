fd659ed238093bd4e1216d63f31e23155b9af6a5
Nicolas <medinamauvecin@gmail.com>
2021-06-03 19:45:44 -0300

    modifica la configuración de la base de datos para utilizar postgresql

--- crud_lol/crud_lol/settings.py
+++ crud_lol/crud_lol/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crud_lol.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'torneo_lol',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
